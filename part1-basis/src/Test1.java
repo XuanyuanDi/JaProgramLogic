da477a87ffd19aba7f5f1167a20673be2e47d901
Divine.H <huangdi201@163.com>
2024-07-10 18:57:40 +0800

    测试github网络问题

---
+++ part1-basis/src/Test1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("dashi");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("213214");
+    }
+}
